6381ae0de0e57454b799e22a3c618fc4499dfba8
Hieu Tran <hieutran106@gmail.com>
2018-02-28 19:48:04 +1000

    Add pagingnation

--- app/store/store.component.ts
+++ app/store/store.component.ts
@@ -9,16 +9,32 @@ import {ProductRepository} from "../model/product.repository";
 })
 export class StoreComponent {
     public selectedCategory = null;
+    public productsPerPage=4;
+    public selectedPage=1;
     constructor(private repository: ProductRepository) {
 
     }
     get products():Product[] {
-        return this.repository.getProducts(this.selectedCategory);
+        let beginIndex=(this.selectedPage-1)*this.productsPerPage;
+        let endIndex = beginIndex+this.productsPerPage;
+        return this.repository.getProducts(this.selectedCategory)
+                .slice(beginIndex,endIndex);
     }
     get categories():string[] {
         return this.repository.getCategories();
     }
     changeCategory(newCategory?: string) {
         this.selectedCategory=newCategory;
+    }
+    changePage(newPage:number) {
+        this.selectedPage=newPage;
+    }
+    changePageSize(newSize:number) {
+        this.productsPerPage = Number(newSize);
+        this.changePage(1);
+    }
+    get pageNumbers():number[] {
+        let pageNumber=Math.ceil(this.repository.getProducts(this.selectedCategory).length/this.productsPerPage);
+        return Array(pageNumber).fill(0).map((value,index) => index+1);
     }
 }
