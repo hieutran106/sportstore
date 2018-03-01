107db847c952429dbd012d9b3a4beda73a864a05
Hieu Tran <hieutran106@gmail.com>
2018-03-01 13:23:04 +1000

    Update cart summary

--- app/model/cart.model.ts
+++ app/model/cart.model.ts
@@ -5,7 +5,44 @@ import {Product} from "./product.model";
 export class Cart {
     public lines: CartLine[] = [];
     public itemCount:number = 0;
-    public cartPrice:number = 0 ;
+    public cartPrice:number = 0;
+
+    addLine(product: Product, quantity:number=1)  {
+        let line= this.lines.find(line => line.product.id==product.id);
+        if (line!=undefined) {
+            line.quality+=quantity;
+        } else {
+            let newLine=new CartLine(product,quantity);
+            this.lines.push(newLine);
+        }
+        this.recalculate();
+        console.log("Hello"+this.itemCount);
+    }
+    updateQuantity(product:Product, quality:number) {
+        let line= this.lines.find(line => line.product.id==product.id);
+        if (line!=undefined) {
+            line.quality=Number(quality);
+        }
+        this.recalculate();
+    }
+    removeLine(id:number) {
+        let index = this.lines.findIndex(line => line.product.id==id);
+        this.lines.splice(index);
+        this.recalculate();
+    }
+    clear() {
+        this.lines=[];
+        this.itemCount=0;
+        this.cartPrice=0;
+    }
+    private recalculate() {
+        this.itemCount=0;
+        this.cartPrice=0;
+        this.lines.forEach(line =>{
+            this.itemCount+=line.quality;
+            this.cartPrice+=line.lineTotal;
+        })
+    }
 }
 export class CartLine {
     constructor(public product:Product, public quality:number) {
--- app/store/store.component.ts
+++ app/store/store.component.ts
@@ -1,6 +1,7 @@
 import {Component} from "@angular/core";
 import {Product} from "../model/product.model";
 import {ProductRepository} from "../model/product.repository";
+import {Cart} from "../model/cart.model";
 
 @Component({
     selector:"store",
@@ -11,7 +12,8 @@ export class StoreComponent {
     public selectedCategory = null;
     public productsPerPage=4;
     public selectedPage=1;
-    constructor(private repository: ProductRepository) {
+    constructor(private repository: ProductRepository,
+                private cart:Cart) {
 
     }
     get products():Product[] {
@@ -37,5 +39,9 @@ export class StoreComponent {
     get pageCount():number {
         let pageCount=Math.ceil(this.repository.getProducts(this.selectedCategory).length/this.productsPerPage);
         return pageCount;
+    }
+
+    addProductToCart(product: Product) {
+        this.cart.addLine(product);
     }
 }
