e9aa30d6fb546a2f46d7abec897657536806a4af
Hieu Tran <hieutran106@gmail.com>
2018-03-02 13:23:12 +1000

    Collect order detail

--- app/store/checkout.component.ts
+++ app/store/checkout.component.ts
@@ -1,5 +1,27 @@
+import { OrderRepository } from './../model/order.repository';
 import { Component } from "@angular/core";
+import { Order } from '../model/order.model';
+import { NgForm } from '@angular/forms';
 @Component({
-    template: `<div><h3 class="bg-info p-a-1">Checkout Component</h3></div>`
+    moduleId: module.id,
+    templateUrl:"checkout.component.html",
+    styleUrls:["checkout.component.css"]
 })
-export class CheckoutComponent { }
+export class CheckoutComponent {
+    orderSent: boolean = false;
+    submitted:boolean = false;
+    constructor(public respository: OrderRepository,
+                public order:Order) {
+
+    }
+    submitOrder(form:NgForm) {
+        this.submitted=true;
+        if (form.valid) {
+            this.respository.saveOrder(this.order).subscribe(order => {
+                this.order.clear();
+                this.orderSent=true;
+                this.submitted=false;
+            })
+        }
+    }
+ }
