6c89f2ed64a7e9d51ccf92658ce5f9b099669691
Hieu Tran <hieutran106@gmail.com>
2018-03-03 09:45:14 +1000

    Add new operation to Product repo

--- app/model/product.repository.ts
+++ app/model/product.repository.ts
@@ -1,3 +1,4 @@
+import { RestDataSource } from './rest.datasource';
 import {Injectable} from "@angular/core";
 import {Product} from "./product.model";
 import {StaticDataSource} from "./static.datasource";
@@ -6,12 +7,30 @@ import {StaticDataSource} from "./static.datasource";
 export class ProductRepository {
     private products: Product[]=[];
     private categories: string[]=[];
-    constructor(private dataSource:StaticDataSource) {
+    constructor(private dataSource:RestDataSource) {
         dataSource.getProducts().subscribe(data => {
             this.products=data;
             this.categories = data.map(p => p.category)
                 .filter((c,index,array) => array.indexOf(c)==index).sort();
         });
+    }
+    saveProduct(product: Product) {
+        if (product.id ==null || product.id==0) {
+            this.dataSource.saveProduct(product)
+                .subscribe(p => this.products.push(p));
+        } else {
+            this.dataSource.updateProduct(product)
+                .subscribe(p => {
+                    this.products.splice(this.products
+                        .findIndex(p => p.id==product.id),1,product);
+                });
+        } 
+    }
+
+    deleteProduct(id:number) {
+        this.dataSource.deleteProduct(id)
+            .subscribe(p => this.products.splice(this.products
+                .findIndex(p => p.id==id),1));
     }
     getProducts(category:string = null):Product[] {
         return this.products.filter(p => category==null || category==p.category);
