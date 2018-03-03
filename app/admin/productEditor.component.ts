b739c0c66dd3aea5915c37bb5495fa944b642213
Hieu Tran <hieutran106@gmail.com>
2018-03-03 14:33:38 +1000

    Implement Product Editor

--- app/admin/productEditor.component.ts
+++ app/admin/productEditor.component.ts
@@ -1,7 +1,28 @@
+import { Router, ActivatedRoute } from '@angular/router';
+import { ProductRepository } from './../model/product.repository';
 import { Component } from '@angular/core';
+import { Product } from '../model/product.model';
+import { NgForm } from '@angular/forms';
 @Component({
-    template: `<div class="bg-warning p-a-1">
-                <h3>Product Editor Placeholder</h3>
-              </div>`
+    moduleId: module.id,
+    templateUrl: "productEditor.component.html"
 })
-export class ProductEditorComponent {}
+export class ProductEditorComponent {
+	editing: boolean = false;
+	product: Product = new Product();
+	constructor(private repo: ProductRepository,
+				private router: Router,
+				activeRoute: ActivatedRoute) {
+    
+        this.editing=activeRoute.snapshot.params["mode"]=="edit";
+        if (this.editing) {
+            let productId=activeRoute.snapshot.params["id"];
+            Object.assign(this.product,repo.getProduct(productId));
+        }
+    }
+    save(form: NgForm) {
+        this.repo.saveProduct(this.product);
+        this.router.navigateByUrl("/admin/main/products");
+    }
+
+}
